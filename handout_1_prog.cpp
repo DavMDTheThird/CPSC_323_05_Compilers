f7d3a14f28882ae80acc65501030178e48da0640
Dav <dm39988@gmail.com>
2024-09-10 16:50:26 -0700

    Creation of stack & print methods, Vector development V0.2

--- handout_1_prog.cpp
+++ handout_1_prog.cpp
@@ -4,18 +4,39 @@
 //          Due date       : 10/09/2024. 
 // Purpose: this program reads an expression in postfix form, evaluates the expression 
 // and displays its value 
-//----------------------------------------------------------------------------------------------------------------- 
+//-------------------------------------------------------------------------------------------------------------
 
 #include <iostream>
+#include <vector>
 
 class stack{
+        std::vector<char> myVec;
 
+    public:
+        stack(std::string expression){
+            for(int i = 0; i < expression.length(); ++i){
+                myVec.push_back(expression[i]);
+                std::cout << expression[i] << std::endl;
+            }
+
+            printVec(myVec);
+        }
+        
+
+        void printVec(std::vector<char> vec_){
+            for(int i = 0; i < myVec.size(); ++i){
+                std::cout<<myVec[i]<<", ";
+            }
+        }
 };
 
-int main(){
+int main(){ 
+    std::cout<< "Give me your expression:";
+    std::string myExpression;
+    std::cin>>myExpression;
+
+    stack myStack(myExpression);
 
 
-
-    std::cout<< "Hola Mundo!!" << std::endl;
     return 0;
 }
