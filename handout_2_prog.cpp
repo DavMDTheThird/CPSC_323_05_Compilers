5285c4014f4ebc62eb68d67cf03a3bf9a29af17a
Dav <dm39988@gmail.com>
2024-09-16 15:50:37 -0700

    [ADD][V0.7] Implement the parcing of operators

--- handout_2_prog.cpp
+++ handout_2_prog.cpp
@@ -11,34 +11,61 @@
 
 class postFix{
     // Vector where the 'stack' is implemented.
-    std::vector<std::string> myVecStr;
-    int answer = 0; 
+    private:
+        std::vector<std::string> myVecStr;
+        int answer = 0; 
 
     public: 
         postFix(std::string expression){
-            // std::cout<<"Trying ("<<expression.length()<<"): "<<expression<<std::endl;
             for(int i = 0; i < expression.length(); i){
                 std::string temp;
                 do{
-                    std::cout<<"now:"<<expression[i]<<std::endl;
                     temp.push_back(expression[i]);
+                    if(checkIfOperator(expression[i])){++i;break;} //If there is an operator at the start
                     ++i;
-                } while ((expression[i] != ' ')&&(expression[i] != '$')&&(expression[i] != '\0'));
+                }while((expression[i] != ' ')&&(expression[i] != '$')&&(expression[i] != '\0')&&!checkIfOperator(expression[i])); //Stop when you find the end, blank space or operator [end of variable]
+
+                myVecStr.push_back(temp);
                 
-                std::cout<<"temp: "<<temp<<std::endl;
-                std::cout<<"-----------------"<<std::endl;
-                // myVecStr.push_back(temp);
-                // printVecStr();
-                if(expression[i] == ' '){++i;}
-                if((expression[i]=='$')||expression[i]=='\0'){break;}
+                if(expression[i] == ' '){++i;} // Skip/ignore blank spaces
+                if((expression[i]=='$')||expression[i]=='\0'){break;} // End of string
+            }
+            // printVecStr();
+            // setValValues();
+            // printVecStr();
+        }
+
+        void solvePostFix(){
+
+        }
+
+        int getAnswer(){
+            return answer;
+        }
+
+        //Check if a character is an operator
+        bool checkIfOperator(char expression){
+            if((expression=='+')||(expression=='-')||(expression=='*')||(expression=='/'))
+                return true;
+            return false;
+        }
+
+        void setValValues(){
+            int i = 0; 
+            for(std::string element : myVecStr){
+                // If it is a number
+                // If its not an operator, it must be a variable
+                if(!isdigit(element[0])&&element[0]!='+' && element[0]!='-' && element[0]!='*' && element[0]!='/'){
+                    std::string value;
+                    std::cout << "Enter the value of " << element << ": ";
+                    std::cin >> value;
+                    myVecStr[i] = value;
+                }
+                i++;
             }
         }
 
         
-        // if(std::isdigit(expression[i])){
-        //     myVec.push_back(expression[i] - '0');
-        //     // std::cout << "num #" <<i <<": "<<expression[i]<< std::endl;
-        // }
         // Method that prints out the compleate stack thats left of the expression
         void printVecStr(){
             for(std::string n : myVecStr){
@@ -49,12 +76,9 @@ class postFix{
 };
 
 int main(int argc, char* argv[]){
-
-    std::cout<< "Enter a postfix expression with $ at the end:";
     std::string myExpression;
+    std::cout<< "Enter a postfix expression with $ at the end:";
     std::getline(std::cin, myExpression);
-    std::cout<<"You said: "<<myExpression<<std::endl;
-
     postFix myPostFix(myExpression);
     // myPostFix.printVecStr();
 
