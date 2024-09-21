1fc97cd47521ccf15cdba2ef95d7acf0037c7bad
Dav <dm39988@gmail.com>
2024-09-20 18:12:50 -0700

    [Start][V0.1] Just the initial commit of the program

---
+++ handout_3_prog_1.cpp
@@ -0,0 +1,19 @@
+//------------------------------------------------------------------------------------------------------------- 
+//          Group names: David Medina Dominguez, Donovan Jabido and Jaiveer Nayen Kapadia 
+//          Assignment     : No.3 
+//          Due date       : 10/25/2024. 
+// Purpose: Write a program to read one token at a time from the given text file and determine whether the token 
+//          is a number, an identifier or a reserved word. 
+//-------------------------------------------------------------------------------------------------------------
+
+#include <iostream>
+
+class Automata{
+
+};
+
+int main(int argc, char* argv[]){
+    
+    
+    return 0;
+}
