f43056e341b10486eb74523b95b9c0d721d6e260
Dav <dm39988@gmail.com>
2024-09-20 18:14:19 -0700

    [Srt&End][V1.0] Creation of class and functions like an automata to resolve if the given input can go through the FA. Documentation needed

---
+++ handout_3_prog_2.cpp
@@ -0,0 +1,87 @@
+//------------------------------------------------------------------------------------------------------------- 
+//          Group names: David Medina Dominguez, Donovan Jabido and Jaiveer Nayen Kapadia 
+//          Assignment     : No.3 
+//          Due date       : 10/25/2024. 
+// Purpose: Write a program to determine whether an input string is accepted or rejected by the grammar. 
+//          Hint, first you have to complete the FA. 
+//-------------------------------------------------------------------------------------------------------------
+
+#include <iostream>
+#include <string>
+
+class Automata{
+    private:
+    // The state has only 4 states:
+    // S = 0, B = 1, C = 2, END = 3
+    int state = 0;
+  
+    bool checkAutomata(char next){
+        switch(state){
+            case 0:
+                if(next == 'a')
+                    return true;
+                else if(next == 'b'){
+                    state = 1;
+                    return true;
+                }
+                else if(next = 'c'){
+                    state = 2;
+                    return true;
+                }
+
+            case 1:
+                if(next == 'b')
+                    return true;
+                else if(next == 'a'){
+                    state = 2;
+                    return true;
+                }
+                else if (next == '$' || next == '\0'){
+                    state = 3;
+                    return true;
+                }
+
+            case 2:
+                if(next == 'a'){
+                    state = 0;
+                    return true;
+                }
+                else if (next == '$' || next == '\0'){
+                    state = 3;
+                    return true;
+                }                    
+
+            default:
+                return false;
+        }
+        return false;
+    }
+    
+    public:
+    Automata(std::string input){
+        for(int i = 0; i < input.length();++i){
+            if(checkAutomata(input[i])){
+                if(state == 3){
+                    std::cout<<"String '"<<input<<"' is accepted"<<std::endl;
+                    break;
+                }
+            }
+            else{
+                std::cout<<"String '"<<input<<"' is NOT accepted"<<std::endl;
+                break;
+            }
+        }
+    }
+
+
+};
+
+
+
+int main(int argc, char* argv[]){
+    for(int i = 1; i < argc; ++i){
+        Automata myAutomata(argv[i]);
+    }
+    
+    return 0;
+}
