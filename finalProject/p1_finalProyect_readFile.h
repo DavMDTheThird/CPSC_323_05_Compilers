009b311b296b278cde21f3fb37009ccd04fdec86
Dav <dm39988@gmail.com>
2024-11-11 17:46:23 -0800

    [ADD][V0.5] First program working, needs comments

--- finalProject/p1_finalProyect_readFile.h
+++ finalProject/p1_finalProyect_readFile.h
@@ -1,54 +1,55 @@
 #include <iostream>
 #include <fstream>  // Include file stream library
-#include <string>
-#include <vector>
 
 #ifndef P1_READFILE_H
 #define P1_READFILE_H
 
 class P1_readFile{
 private:
-std::vector<std::vector<std::string>> myFileWords;
 
 public:
     P1_readFile(std::string fileName = "final.txt"){
-        std::ifstream file(fileName);
-        if (!file){std::cout << "Unable to open file\n";}
+        std::string fileTitle;
+        int i = 0;
+        while(fileName[i] != '.'){
+            fileTitle += fileName[i];
+            ++i;
+        }
+        fileTitle += "f24.txt";
 
-        std::vector<std::string> myLineWords;
-        std::string line;
-        while (std::getline(file, line)){  // Read the file line by line
-            myLineWords.clear();
-            std::string word = "";
-            for(int i = 0; i < line.length(); ++i){
-                if((line[i] == ' ' && word != "")||line[i] == ';'){
-                    line[i]==';'?word+=line[i]:word;
+        std::ofstream fileOut(fileTitle);
+        
+        std::ifstream fileIn(fileName);
+        if (!fileIn){std::cout << "Unable to open file\n";}
+        if (!fileOut){std::cout << "Unable to create file\n";}
 
-                    myLineWords.push_back(word);
-                    std::cout<<"word: "<<word<<std::endl;
-                    word = "";
-                }
-                else if (line[i] != ' '){
-                    // std::cout<<line[i]<<std::endl;
-                    word += line[i];
+        std::string word;
+        bool comment = false;
+        while(fileIn >> word){
+            if(word[0] == '('){
+                if(word[1] == '*'){
+                    comment = true;
                 }
             }
-            // std::cout<<std::endl;
-            this->myFileWords.push_back(myLineWords);
-        }
-        std::cout<<"-----------"<<std::endl;
-        printVec();
-
-        file.close();  // Close the file
-    }
-
-    void printVec(){
-        for (const auto& line : this->myFileWords) {
-            for (const auto& word : line) {
-                std::cout << word << " ";
+            if(!comment){
+                fileOut << word;
+                fileOut << " ";
+                // std::cout << word << " ";
+                if(word[word.length()-1] == ';'){
+                fileOut << "\n";
+                }
             }
-            std::cout << std::endl;
+            else{
+                if(word[word.length()-1] == ')'){
+                    if(word[word.length()-2] == '*'){
+                        comment = false;
+                    }
+                }
+            }
         }
+
+        fileIn.close();
+        fileOut.close();
     }
 };
 #endif
