e46e68e57c892f21f9e20fc8823cf335e0e3c159
jaiveer09 <jaiveerkapadia18@gmail.com>
2024-12-01 21:46:35 -0800

    Revert "Part 3 added but not fully sure"
    This reverts commit 8bb07397839c6dc38f2af469ecd9b6180b2dc0fe.

--- finalProject/finalProject.cpp
+++ finalProject/finalProject.cpp
@@ -1,18 +1,10 @@
 #include <iostream>
 #include "finalProject_readFile.h"
 #include "finalProject_translation.h"
-#include "tokenizer.h"
-#include "syntax_analyzer.h"
 
 int main(int argc, char* argv[]){
     ReadFile myP1_rf;
     TranslateFile myP1_t(myP1_rf.getFileName());
-    Tokenizer          tokenizer( myP1_rf.getFileName() );
-    std::vector<Token> tokens = tokenizer.tokenize();
-
-    // Create a SyntaxAnalyzer object and analyze the tokens
-    SyntaxAnalyzer syntaxAnalyzer( tokens );
-    syntaxAnalyzer.analyze();
 
     return 0;
 }
